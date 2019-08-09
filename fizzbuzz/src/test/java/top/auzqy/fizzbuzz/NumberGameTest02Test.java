b8fb2a9428a12801697e3c8c2c568ab04e724dd7
au <auzhouqingyu@163.com>
2019-08-09 23:37:50 +0800

    add note

--- fizzbuzz/src/test/java/top/auzqy/fizzbuzz/NumberGameTest02Test.java
+++ fizzbuzz/src/test/java/top/auzqy/fizzbuzz/NumberGameTest02Test.java
@@ -5,6 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+/**
+ * description:  FizzBuzz 小游戏
+ *      初始需求：
+ *          从 1 开始报数，遇到 3 的倍数就报 Fizz，
+ *          遇到 5 的倍数就报 Buzz，
+ *          遇到即是 3 的倍数，又是 5 的倍数的时候，就报 FizzBuzz。
+ *
+ *      中途更改需求：
+ *          基于上述规则，拟报数字遇到包含 3 这个数字的情况，也报 Fizz，
+ *          拟报数字遇到包含 5 这个数字的情况，也报 Buzz，
+ *          拟报数字遇到即包含 3 这个数字，又包含 5 这个数字的情况，
+ *          也报 FizzBuzz，
+ *
+ * createTime: 2019-08-09 23:31
+ * @author au
+ */
 public class NumberGameTest02Test {
 
     @ParameterizedTest

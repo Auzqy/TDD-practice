f4c41e6c0e300edc2d8efb94904cd4bf8226db17
au <auzhouqingyu@163.com>
2019-08-09 15:49:55 +0800

    add JUnit5 dependency in pom and make a test

---
+++ fizzbuzz/src/test/java/top/auzqy/fizzbuzz/SimpleTest.java
@@ -0,0 +1,14 @@
+package top.auzqy.fizzbuzz;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+    @Test
+    void testNoUse() {
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(3,2+1);
+    }
+}

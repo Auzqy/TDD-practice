c292e0bf644a2555e6b9b346064ec63cea9f92b5
au <auzhouqingyu@163.com>
2019-10-24 17:22:23 +0800

    test args_tdd with WIP

---
+++ args/src/main/java/top/auzqy/args/test03/Schemas.java
@@ -0,0 +1,7 @@
+package top.auzqy.args.test03;
+
+public class Schemas {
+    public Schemas(String schema) {
+
+    }
+}
---
+++ args/src/test/java/top/auzqy/args/test03/SchemaTest.java
@@ -0,0 +1,30 @@
+package top.auzqy.args.test03;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * description:  schema test
+ * createTime: 2019-10-24 17:12
+ * @author au
+ */
+public class SchemaTest {
+
+    static Stream<Arguments> schema_expected_paraName_paraValue() {
+        return Stream.of(
+                Arguments.of("l:bool","true","l","true"),
+                Arguments.of("l:bool","false","l","false"),
+                Arguments.of("l:bool","true","l",null)
+        );
+    }
+    
+    @ParameterizedTest
+    @MethodSource("schema_expected_paraName_paraValue")
+    void should_(String schema, String expected,
+                 String paraName, String paraValue) {
+        Schemas schemas = new Schemas(schema);
+    }
+}

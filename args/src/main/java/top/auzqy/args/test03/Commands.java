4b437b76c1b1e97ec7aab4d09f8dd90deb23b006
au <auzhouqingyu@163.com>
2019-10-24 18:23:26 +0800

    test args_tdd with WIP

---
+++ args/src/main/java/top/auzqy/args/test03/Commands.java
@@ -0,0 +1,23 @@
+package top.auzqy.args.test03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * description:  命令行参数解析
+ * createTime: 2019-10-24 18:19
+ * @author au
+ */
+public class Commands {
+    Map<String, String> commands;
+
+    public Commands(String commandConfig) {
+        commands = new HashMap<>();
+
+    }
+
+
+    public Object getValue(String paraName) {
+        return null;
+    }
+}
--- args/src/test/java/top/auzqy/args/test03/CommandTest.java
+++ args/src/test/java/top/auzqy/args/test03/CommandTest.java
@@ -1,5 +1,6 @@
 package top.auzqy.args.test03;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,8 +11,9 @@ public class CommandTest {
     
     static Stream<Arguments> command_excepted_paraName_paraValue() {
         return Stream.of(
-                Arguments.of("-l -p 8081 -d /usr/local", true, "l", null),
-                Arguments.of("-l true -p 8081 -d /usr/local", true, "l", "true")
+                Arguments.of("-l -p 8081 -d /usr/local", true, "l"),
+                Arguments.of("-l true -p 8081 -d /usr/local", true, "l")
+//                Arguments.of("-l true -p 8081 -d /usr/local", 8081, "p")
         );
     }
     
@@ -19,8 +21,8 @@ public class CommandTest {
     @MethodSource("command_excepted_paraName_paraValue")
     void should_command_excepted_paraName_paraValue(
             String command, Object expected,
-            String paraName, String paraValue) {
-
-        
+            String paraName) {
+//        Commands commands = new Commands(command);
+//        Assertions.assertEquals(expected,commands.getValue(paraName));
     }
 }

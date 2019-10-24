4a478bbf4015afe29cd231ae8a82d43bab7cb829
au <auzhouqingyu@163.com>
2019-10-24 18:09:45 +0800

    test args_tdd with WIP

--- args/src/test/java/top/auzqy/args/test03/SchemaTest.java
+++ args/src/test/java/top/auzqy/args/test03/SchemaTest.java
@@ -16,9 +16,9 @@ public class SchemaTest {
 
     static Stream<Arguments> schema_expected_paraName_paraValue() {
         return Stream.of(
-                Arguments.of("l:bool","true","l","true"),
-                Arguments.of("l:bool","false","l","false"),
-                Arguments.of("l:bool","false","l",null),
+                Arguments.of("l:bool",true,"l","true"),
+                Arguments.of("l:bool",false,"l","false"),
+                Arguments.of("l:bool",false,"l",null),
 
                 Arguments.of("l:int","1","l","1"),
                 Arguments.of("l:int","-9","l","-9"),
@@ -26,13 +26,22 @@ public class SchemaTest {
                 Arguments.of("l:str","abc","l","abc")
         );
     }
-    
+
     @ParameterizedTest
     @MethodSource("schema_expected_paraName_paraValue")
-    void should_(String schema, String expected,
+//    void should_(String schema, String expected,
+//                 String paraName, String paraValue) {
+    void should_(String schema, Object expected,
                  String paraName, String paraValue) {
         Schemas schemas = new Schemas(schema);
+        /*
+            其实测试不光是测值，同时也是测类型
+            这种 toString() 的方式是达不到
+            这种目的的
+         */
+//        Assertions.assertEquals(expected,
+//                schemas.getValue(paraName,paraValue).toString());
         Assertions.assertEquals(expected,
-                schemas.getValue(paraName,paraValue).toString());
+                schemas.getValue(paraName,paraValue));
     }
 }

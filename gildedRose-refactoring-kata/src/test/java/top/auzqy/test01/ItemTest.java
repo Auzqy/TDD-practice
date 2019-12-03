3f87796909a8217812d2270736ea9b442dc402c7
au <auzhouqingyu@163.com>
2019-12-03 14:17:56 +0800

    refactor gildedRose test01

---
+++ gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/ItemTest.java
@@ -0,0 +1,13 @@
+package top.auzqy.test01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void should_double_increase_backstage_pass_value_when_getting_close_to_expiration() {
+        
+    }
+}

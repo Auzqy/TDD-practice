7b5f44cd5e72b9de840c8a0ae26da06ef5512675
au <auzhouqingyu@163.com>
2019-12-03 12:27:11 +0800

    refactor gildedRose test01

--- gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/GildedRoseSafetyNet.java
+++ gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/GildedRoseSafetyNet.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
@@ -15,11 +16,11 @@ import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 public class GildedRoseSafetyNet {
 
     @Test
-    void should_always_align_with_baseline() {
+    void should_always_align_with_baseline() throws IOException {
         String output = TextTestFixture.getBaseLine();
         String baseline = Files.asCharSource(
                 new File("src/test/java/top/auzqy/test01/baseline.txt"),
-                Charsets.UTF_8).toString();
+                Charsets.UTF_8).read();
         System.out.println("output = " + output);
         System.out.println("baseline = " + baseline);
 //        Assertions.assertEquals(output, baseline);

710f77f9655050c65f1581a70863a2278fd0125d
au <auzhouqingyu@163.com>
2019-12-03 14:26:25 +0800

    refactor gildedRose test01

--- gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/ItemTest.java
+++ gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/ItemTest.java
@@ -1,13 +1,15 @@
 package top.auzqy.test01;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
     @Test
     void should_double_increase_backstage_pass_value_when_getting_close_to_expiration() {
-        
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+        item.passOneDay();
+        MatcherAssert.assertThat(item.quality, Is.is(21));
     }
 }

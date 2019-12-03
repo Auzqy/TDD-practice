91bbd6fd6879a8a087c26df93eda67072299d644
au <auzhouqingyu@163.com>
2019-12-03 11:52:52 +0800

    test args_tdd with WIP

---
+++ gildedRose-refactoring-kata/src/test/java/top/auzqy/test01/GildedRoseSafetyNet.java
@@ -0,0 +1,11 @@
+package top.auzqy.test01;
+
+import org.junit.jupiter.api.Test;
+
+public class GildedRoseSafetyNet {
+    
+    @Test
+    void should_always_align_with_baseline() {
+        
+    }
+}
